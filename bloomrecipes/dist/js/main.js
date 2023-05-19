4d9fe91b9eea9fdbb30b0ccdd14a82ae4b8cc674
MoniAF <114707420+MoniAF@users.noreply.github.com>
2023-05-18 23:00:31 -0600

    Actualización 18
    Funcionamiento del buscar de recipes.html unicamente por busqueda de nombre de receta.

--- bloomrecipes/dist/js/main.js
+++ bloomrecipes/dist/js/main.js
@@ -1,7 +1,8 @@
 const app = Vue.createApp({
     data() {
         return {
-            selectedIndex:0,
+            message: "All Recipes",
+            searchTerm: "",
             recipes:[
                 /*{id: 1, name:"Chocolate and Walnut Brownies", image:"./img/chocolate.jpg", prepTime:15, cookTime:25, totalTime:40, servings: 12, difficulty:"Intermediate", category:"Desserts", occasion:"All", description:"These chocolate and walnut brownies are the perfect dessert for any occasion. They're rich and decadent, with an intense chocolate flavor and crunchy chopped walnuts. They're easy to make and can be enjoyed on their own or with a scoop of vanilla ice cream. You can make them ahead of time and store them in an airtight container in the fridge to enjoy for several days. These brownies will definitely satisfy any chocolate craving!", ingredients:"1/2 cup (1 stick) unsalted butter,*1 cup granulated sugar,*2 large eggs,*1 teaspoon vanilla extract,*1/2 cup all-purpose flour,*1/3 cup unsweetened cocoa powder,*1/4 teaspoon salt,*1/4 teaspoon baking soda,*1/2 cup chopped walnuts.", instructions:"Preheat the oven to 350°F (175°C). Grease an 8-inch (20 cm) square baking pan and line it with parchment paper. *In a medium saucepan over medium heat, melt the butter. Add the sugar and mix until well combined. Remove from heat and let cool for a few minutes.*Add the eggs and vanilla to the butter-sugar mixture and beat until well combined.*In a separate bowl, sift together the flour, cocoa powder, salt, and baking soda. Add the dry ingredients to the egg mixture and mix until well combined.*Add the chopped walnuts and mix gently.*Pour the mixture into the prepared baking pan and bake for 25-30 minutes or until a toothpick inserted into the center comes out clean.*Let cool in the pan for 10 minutes before cutting into squares and serving.", likes:220},
                 {id: 2, name:"Creamy Tomato Soup", image:"./img/chocolate.jpg", prepTime:15, cookTime:25, totalTime:40, servings: 12, difficulty:"Intermediate", category:"Desserts", occasion:"All", description:"These chocolate and walnut brownies are the perfect dessert for any occasion. They're rich and decadent, with an intense chocolate flavor and crunchy chopped walnuts. They're easy to make and can be enjoyed on their own or with a scoop of vanilla ice cream. You can make them ahead of time and store them in an airtight container in the fridge to enjoy for several days. These brownies will definitely satisfy any chocolate craving!", ingredients:"1/2 cup (1 stick) unsalted butter.*1 cup granulated sugar.*2 large eggs.*1 teaspoon vanilla extract.*1/2 cup all-purpose flour.*1/3 cup unsweetened cocoa powder.*1/4 teaspoon salt.*1/4 teaspoon baking soda.*1/2 cup chopped walnuts", instructions:"Preheat the oven to 350°F (175°C). Grease an 8-inch (20 cm) square baking pan and line it with parchment paper. *In a medium saucepan over medium heat, melt the butter. Add the sugar and mix until well combined. Remove from heat and let cool for a few minutes.*Add the eggs and vanilla to the butter-sugar mixture and beat until well combined.*In a separate bowl, sift together the flour, cocoa powder, salt, and baking soda. Add the dry ingredients to the egg mixture and mix until well combined.*Add the chopped walnuts and mix gently.*Pour the mixture into the prepared baking pan and bake for 25-30 minutes or until a toothpick inserted into the center comes out clean.*Let cool in the pan for 10 minutes before cutting into squares and serving.", likes:135},
@@ -35,7 +36,6 @@ const app = Vue.createApp({
         }
     },
     mounted() {
-
         //agregar Dessert
         axios({
             method: 'get',
@@ -158,7 +158,7 @@ const app = Vue.createApp({
                             name: element.strMeal,
                         })
                 });
-                
+
                 for (let index = 0; index < this.recipes.length; index++) {
                     axios({
                         method: 'get',
@@ -202,6 +202,39 @@ const app = Vue.createApp({
         });*/
     },
     methods: {
-        
+        onClickSearchRecipe(){
+            axios({
+                method: 'get',
+                url:'https://www.themealdb.com/api/json/v1/1/search.php?s='+this.searchTerm
+               })
+            .then(
+                (response) => {
+                    let items = response.data.meals;
+                    this.recipes = [];
+                    console.log(items);
+                    console.log("palabra" +this.searchTerm);
+                    if(items == null){
+                        this.message= "No se ha encontrado "+this.searchTerm;
+                    }else{
+                        this.message= "Search results for "+this.searchTerm;
+                    }
+                    
+
+                    items.forEach(element => {
+                        this.recipes.push({ 
+                                id: element.idMeal,
+                                image: element.strMealThumb,
+                                name: element.strMeal,
+                                category : element.strCategory,
+                                description : "This recipe is a delightful combination of flavors and textures that will leave you wanting more. With fresh ingredients and a careful selection of spices, this recipe offers a unique culinary experience. Start by mixing the main ingredients in a bowl and seasoning them with a pinch of salt and pepper to enhance the flavors. Then, heat a skillet over medium heat and add a drizzle of oil to brown the ingredients. As they cook, the irresistible aroma fills the kitchen, creating an anticipation that is hard to resist.",
+                                difficulty : "Intermediate"
+                            })
+                    });
+                }
+            )
+            .catch(
+                error => console.log(error)
+            );
+        }
     }
 })
